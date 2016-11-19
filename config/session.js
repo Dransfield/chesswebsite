5fd2a8080e9c32cdf6e080f0f77f101addd93b5f
root <slenkar@gmail.com>
2016-11-19 13:35:38 +0800

    h

--- config/session.js
+++ config/session.js
@@ -67,7 +67,7 @@ module.exports.session = {
   *                                                                          *
   ***************************************************************************/
 
-   adapter: 'mongo',
+   adapter: 'connect-mongo',
    host: 'localhost',
    port: 27017,
    db: 'chessdb',
